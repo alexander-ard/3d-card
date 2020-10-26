c2d8dd8b182dc783ae98155c6de3005c7c958b1c
alexander-ard <alexanderardmirola@gmail.com>
2020-10-25 20:44:17 -0500

    Updates for active state in size selection

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const card = document.querySelector('.card');
 const container = document.querySelector('.container');
-
 const title = document.querySelector('.title');
 const sneaker = document.querySelector('.sneaker img');
 const purchase = document.querySelector('.purchase button');
 const description = document.querySelector('.info h3');
 const sizes = document.querySelector('.sizes');
+const sizesInputs = document.querySelectorAll('.sizes input');
 
 container.addEventListener('mouseover', (e) => {
     let xAxis = ((window.innerWidth / 2) - e.pageX) / 25;
@@ -19,7 +19,7 @@ container.addEventListener('mouseenter', e => {
 
     title.style.transform = 'translateZ(150px)';
     sneaker.style.transform = 'translateZ(200px) rotateZ(-45deg)';
-    purchase.style.transform = 'translateZ(75px)';
+    purchase.style.transform = 'translateZ(150px)';
     description.style.transform = 'translateZ(75px)';
     sizes.style.transform = 'translateZ(75px)';
 });
@@ -33,4 +33,15 @@ container.addEventListener('mouseleave', (e) => {
     purchase.style.transform = 'translateZ(0px)';
     description.style.transform = 'translateZ(0px)';
     sizes.style.transform = 'translateZ(0px)';
-})
+});
+
+sizesInputs.forEach(el => {
+    el.addEventListener('change', e => {
+        document.querySelectorAll('.sizes label').forEach(l => {
+            l.classList.remove('active');
+        });
+
+        const label = document.querySelector(`label[for='${e.target.id}']`);
+        label.classList.add('active');
+    });
+});
