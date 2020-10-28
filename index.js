1b738fefa2537dfff87ad5b8732b958e3fe55328
alexander-ard <alexanderardmirola@gmail.com>
2020-10-27 19:41:31 -0500

    Animate circle

--- index.js
+++ index.js
@@ -5,34 +5,35 @@ const sneaker = document.querySelector('.sneaker img');
 const purchase = document.querySelector('.purchase button');
 const description = document.querySelector('.info h3');
 const sizes = document.querySelector('.sizes');
+const circle = document.querySelector('.circle');
 const sizesInputs = document.querySelectorAll('.sizes input');
 
 container.addEventListener('mousemove', (e) => {
     let xAxis = ((window.innerWidth / 2) - e.pageX) / 25;
     let yAxis = ((window.innerHeight / 2) - e.pageY) / 25;
-
+    
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-})
-
-container.addEventListener('mouseenter', e => {
-    card.style.transition = 'none';
-
     title.style.transform = 'translateZ(150px)';
     sneaker.style.transform = 'translateZ(200px) rotateZ(-45deg)';
     purchase.style.transform = 'translateZ(150px)';
     description.style.transform = 'translateZ(75px)';
     sizes.style.transform = 'translateZ(75px)';
+    circle.style.transform = `rotate(180deg)`;
+
+    card.style.transition = 'none';
 });
 
 container.addEventListener('mouseleave', (e) => {
     card.style.transition = 'all 0.5s ease';
-    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
 
+    card.style.transform = `rotateY(0deg) rotateX(0deg)`;
     title.style.transform = 'translateZ(0px)';
     sneaker.style.transform = 'translateZ(0px) rotateZ(0deg)';
     purchase.style.transform = 'translateZ(0px)';
     description.style.transform = 'translateZ(0px)';
     sizes.style.transform = 'translateZ(0px)';
+    circle.style.transform = `rotate(0deg)`;
+
 });
 
 sizesInputs.forEach(el => {
